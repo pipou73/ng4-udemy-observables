5cc105687203f3c5b927278a3fd5c715094013a4
vesin philippe <pvesin@sapiens.biz>
2018-01-09 12:39:40 +0100

    finish-observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {}
+  user1Activated= false;
+  user2Activated= false;
+
+  constructor(private usersService: UsersService) {}
 
   ngOnInit() {
-
+    this.usersService.userActivated.subscribe((id: number) => {
+      if (id === 1) {
+        this.user1Activated = true;
+      } else if (id === 2) {
+        this.user2Activated = true;
+      }
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import {AppRoutingModule} from './app-routing.module'
+import {UsersService} from "./users.service";
 
 @NgModule({
     declarations: [
@@ -17,7 +18,7 @@ import {AppRoutingModule} from './app-routing.module'
         BrowserModule,
         AppRoutingModule
     ],
-    providers: [],
+    providers: [UsersService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
